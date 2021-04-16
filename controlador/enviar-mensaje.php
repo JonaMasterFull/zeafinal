c1c0572d334e42b90d8fde4524c5c5a4c46efb14
Royce667 <jonytello76@gmail.com>
2021-04-16 15:13:33 -0500

    Correccion

--- controlador/enviar-mensaje.php
+++ controlador/enviar-mensaje.php
@@ -34,7 +34,7 @@ if(isset($_POST['btn-enviar'])){
             $mail­->Subject = "Mensaje Prueba";
             $mail­->MsgHTML($ContenidoMensaje);
 
-            $address = "pnuevas466@gmail.com";//Correo A enviar
+            $address = "karengarcia13303@gmail.com";//Correo A enviar
 
 
             $mail­->AddAddress($address, "Mensaje Prueba");
