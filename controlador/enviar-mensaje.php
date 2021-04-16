f3bb0681ef0ddcd07538f39efc9257903e4f27a8
Royce667 <jonytello76@gmail.com>
2021-04-15 21:36:46 -0500

    Mensajes de cambios

--- controlador/enviar-mensaje.php
+++ controlador/enviar-mensaje.php
@@ -4,10 +4,11 @@
 if(isset($_POST['btn-enviar'])){
 
         $id = $_POST['id'];
-        $destinatario = $_POST['email'];
+        $Email = $_POST['email'];
         $Mensajes = $_POST['mensaje'];
 
         // Datos para el correo
+        $destinatario = "jonytello76@gmail.com";
         $asunto = "Pregunta de Cliente";
         $header = "El mensaje se ha recibido con exito" . "\r\n";
         $header.= "Inicio session en el Administrador de la Pagina";
@@ -15,17 +16,17 @@ if(isset($_POST['btn-enviar'])){
         try{
             
             require_once '../database/conexion.php';
-            $sql = "INSERT INTO mensaje VALUES (Null,'$destinatario','$Mensajes','$id')";
+            $sql = "INSERT INTO mensaje VALUES (Null,'$Email','$Mensajes','$id')";
 
             $ejecutar = mysqli_query($conectar,$sql);
 
             if(!$ejecutar){
-                mail($destinatario, $asunto, $Mensajes,$header);
+                mail($destinatario, $asunto, $Mensajes, $header);
                 header("Location: ../mensaje-enviado.php");
 
             }else{
                
-                mail($destinatario, $asunto, $Mensajes,$header);
+                mail($destinatario, $asunto, $Mensajes, $header);
                 if($mail){
                     header("Location: ../mensaje-enviado.php");
                 }else{
