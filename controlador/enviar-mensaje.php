aa0cc7ce877d43f5f4c7c7af082b2c2f711afabf
Royce667 <jonytello76@gmail.com>
2021-04-16 01:01:18 -0500

    ads a

--- controlador/enviar-mensaje.php
+++ controlador/enviar-mensaje.php
@@ -16,47 +16,9 @@ if(isset($_POST['btn-enviar'])){
             $FromEmail = "valverdefrancisco907@gmail.com". ",";
             $FromEmail.= "perezaguirre414@gmail.com";
 
-            /*
-            $host = "smtp.live.com";
-            $port = "587";
-            $SMTPAuth = "login";
-            $SMTPSecure = "tls";
-            $Password = "56899Jona2010";// Contraseña del correo a mandar
-
-            $mail = new PHPMailer\PHPMailer\PHPMailer();
-            $mail->isSMTP();
-            $mail->SMTPDebug = 0;
-            $mail->Host = $host;
-            $mail->Port = $port;
-            $mail->SMTPAuth = $SMTPAuth;
-            $mail->SMTPSecure = $SMTPSecure;
 
 
-            $mail->Username = $FromEmail;
-            $mail->Password = $Password;
-            
-            $mail->setFrom($FromEmail, $FromName);
-            $mail->addAddress($Email);
-            $mail->isHTML(true);
-            
-            $mail->Subject = $subject;
-           
-            $mail->Body = $Mensajes;
-            $mail->send();
-*/
-
-$cabeceras  = 'MIME-Version: 1.0' . "\r\n";
-$cabeceras .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-
-// Cabeceras adicionales
-$cabeceras .= 'To: Mary <mary@example.com>, Kelly <kelly@example.com>' . "\r\n";
-$cabeceras .= 'From: Recordatorio <cumples@example.com>' . "\r\n";
-$cabeceras .= 'Cc: birthdayarchive@example.com' . "\r\n";
-$cabeceras .= 'Bcc: birthdaycheck@example.com' . "\r\n";
-
-
-
-mail($FromEmail, $título, $Mensajes, $cabeceras);
+            mail($FromEmail, $título, $Mensajes);
                 if(!mail){
 
                     header("Location: ../index.php");
